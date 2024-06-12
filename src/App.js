88bd909f287562aedadf456393109d760e0bd917
milindkolambe <milind.kolambe@cumminscollege.in>
2024-06-12 09:44:56 +0530

    First React app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div classNameNameName="App">
+        <h1> Jai Shrikrishna</h1>
+        <Navbar title="MyNavbar" /> {/* We're calling Navbar and will pass actual parameters (props) */}
+      </div>
+    </>
   );
 }
 
